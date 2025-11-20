80884713de929205373174c2d0414312489b80c3
ebjai <eddiebarberiz@gmail.com>
2025-11-19 19:54:54 -0500

    Simplify body class in RootLayout component

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,9 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-grid text-slate-100 antialiased">
-        {children}
-      </body>
+      <body className="min-h-screen">{children}</body>
     </html>
   );
 }
